55314c19d466210f02a031bd8b9c9081e67e06f1
Hiroki Omae <pegion6@gmail.com>
2013-02-22 20:10:25 +0900

    ポジション固定ユーティリティを追加

---
+++ vjkit/Assets/VJKit/Script/Utility/VJPositionFreeze.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("VJKit/Utility/Freeze Position")]
+public class VJPositionFreeze : MonoBehaviour {
+
+	public bool freezePosition = true;
+	public bool freezeRotation = false;
+	public bool freezeScale = false;
+
+	private Vector3 savedPosition;
+	private Quaternion savedRotation;
+	private Vector3 savedScale;
+
+	// Use this for initialization
+	void Start () {
+		savedPosition = transform.position;
+		savedRotation = transform.rotation;
+		savedScale    = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(freezePosition)
+			transform.position = savedPosition;
+		if(freezeRotation)
+			transform.rotation = savedRotation;
+		if(freezeScale)
+			transform.localScale = savedScale;
+	}
+}
