c63eca179a5344dc8c98552f2243454e68552dcd
Hiroki Omae <pegion6@gmail.com>
2013-03-05 13:12:37 +0900

    implemented scene speed modifier

---
+++ vjkit/Assets/VJKit/Editor/VJSceneSpeedModifierEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(VJSceneSpeedModifier))]
+public class VJSceneSpeedModifierEditor : VJBaseModifierEditor 
+{
+    public VJSceneSpeedModifierEditor()
+    {
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
---
+++ vjkit/Assets/VJKit/Script/Modifier/VJSceneSpeedModifier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("VJKit/Modifiers/Scene Speed")]
+public class VJSceneSpeedModifier : VJBaseModifier {
+
+	public bool normalizeWhenDispabled;
+	public float normalValue = 1.0f;
+
+	public override void VJPerformAction(GameObject go, float value) {	
+
+		if(isModifierEnabled) {
+			Time.timeScale = Mathf.Max(0.0f, value);
+		} else {
+			if(normalizeWhenDispabled) {
+				Time.timeScale = normalValue;
+			}
+		}
+	}
+	
+	public void OnDisable() {
+		if(normalizeWhenDispabled) {
+			Time.timeScale = normalValue;
+		}
+	}
+}
