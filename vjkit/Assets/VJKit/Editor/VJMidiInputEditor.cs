adecc8300e81acd1952f0a3427737b09f0dd04d8
Hiroki Omae <pegion6@gmail.com>
2013-10-31 16:55:40 +0900

    adding license comment

--- vjkit/Assets/VJKit/Editor/VJMidiInputEditor.cs
+++ vjkit/Assets/VJKit/Editor/VJMidiInputEditor.cs
@@ -1,5 +1,62 @@
-// Unity MIDI Input plug-in / Inspector
-// By Keijiro Takahashi, 2013
+/*
+fuZe vjkit
+
+Copyright (C) 2013 Unity Technologies Japan, G.K.
+
+Permission is hereby granted, free of charge, to any 
+person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the 
+Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject 
+to the following conditions: The above copyright notice and 
+this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+OTHER DEALINGS IN THE SOFTWARE.
+
+Special Thanks:
+The original software design and architecture of fuZe vjkit 
+is inspired by Visualizer Studio, created by Altered Reality 
+Entertainment LLC.
+
+VJMidiInputEditor.cs
+based on 
+	Unity MIDI Input plug-in / C# interface
+	By Keijiro Takahashi, 2013
+	https://github.com/keijiro/unity-midi-input
+
+Unity MIDI Input plug-in 
+License:
+Copyright (C) 2013 Keijiro Takahashi
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+IN THE SOFTWARE.
+  */
+
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
